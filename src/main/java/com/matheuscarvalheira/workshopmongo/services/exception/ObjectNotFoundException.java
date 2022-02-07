ceb110fd3ec8c14b774c089595c2842371824ca4
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-07 16:09:47 -0300

    Creating a custom Exception

---
+++ src/main/java/com/matheuscarvalheira/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.matheuscarvalheira.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+
+
+}
