631c9099216e09aaa73f80a592a58fd06a29074e
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-07 16:08:08 -0300

    Embedding findById method through repository

--- src/main/java/com/matheuscarvalheira/workshopmongo/services/UserService.java
+++ src/main/java/com/matheuscarvalheira/workshopmongo/services/UserService.java
@@ -2,8 +2,12 @@ package com.matheuscarvalheira.workshopmongo.services;
 
 import com.matheuscarvalheira.workshopmongo.domain.User;
 import com.matheuscarvalheira.workshopmongo.repository.UserRepository;
+import com.matheuscarvalheira.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -19,5 +23,15 @@ public class UserService {
         //retorna todos os objetos do banco
         return repo.findAll();
     }
+
+    public User findById(String id){
+
+        Optional<User> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+
+    }
+
 
 }
