701178453a5ac0bfccd93089d04f7d003f1cbbb0
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-09 18:36:35 -0300

    creation of delete method in service layer

--- src/main/java/com/matheuscarvalheira/workshopmongo/services/UserService.java
+++ src/main/java/com/matheuscarvalheira/workshopmongo/services/UserService.java
@@ -38,9 +38,15 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public void delete(String id){
+        findById(id); //realiza uma busca primeiro
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
 
 
 }
