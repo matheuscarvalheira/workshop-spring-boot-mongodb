03e863e2441571fcb82846376c18d7329874cb53
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-11 15:15:06 -0300

    creating connection to the bank in the context of the post

---
+++ src/main/java/com/matheuscarvalheira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.matheuscarvalheira.workshopmongo.repository;
+
+import com.matheuscarvalheira.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+}
