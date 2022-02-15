698b28b78cf63db42892882cccdeb8fab2975618
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-15 12:02:38 -0300

    Instantiating comments and relating to posts

--- src/main/java/com/matheuscarvalheira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/matheuscarvalheira/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.matheuscarvalheira.workshopmongo.config;
 import com.matheuscarvalheira.workshopmongo.domain.Post;
 import com.matheuscarvalheira.workshopmongo.domain.User;
 import com.matheuscarvalheira.workshopmongo.dto.AuthorDTO;
+import com.matheuscarvalheira.workshopmongo.dto.CommentDTO;
 import com.matheuscarvalheira.workshopmongo.repository.PostRepository;
 import com.matheuscarvalheira.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse( "22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO( "Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
