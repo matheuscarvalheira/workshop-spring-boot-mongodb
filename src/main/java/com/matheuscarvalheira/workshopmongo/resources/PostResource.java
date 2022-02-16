81005d6422cf0c99a735d9648c63bdacc02cb112
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-16 16:08:22 -0300

    exposing endpoint to full string search

--- src/main/java/com/matheuscarvalheira/workshopmongo/resources/PostResource.java
+++ src/main/java/com/matheuscarvalheira/workshopmongo/resources/PostResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -36,6 +37,23 @@ public class PostResource {
         text  = URL.decodeParam(text);
         //retorna a  lista de posts com a texto coincidindo com o  que eu dei no parametro
         List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
+    }
+
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue = "") String text,
+            @RequestParam(value="minDate", defaultValue = "") String minDate,
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate
+    ) {
+
+        text  = URL.decodeParam(text);
+        Date min  = URL.convertDate(minDate, new Date(0L));
+        Date max  = URL.convertDate(maxDate, new Date());
+
+        List<Post> list = service.fullSearch(text,min,max);
 
         return ResponseEntity.ok().body(list);
     }
